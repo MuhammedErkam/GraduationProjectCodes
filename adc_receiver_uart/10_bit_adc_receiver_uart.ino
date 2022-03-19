b96502d2b84977d3b56fe85f07e876430ea90ced
MuhammedErkam <muhammed.erkam1998@hotmail.com>
2022-03-19 17:18:48 +0300

    Receiver PWM Setting is changed

--- adc_receiver_uart/10_bit_adc_receiver_uart.ino
+++ adc_receiver_uart/10_bit_adc_receiver_uart.ino
@@ -1,18 +1,18 @@
-const int analog_out = 9;
+const int analog_out = 3;
 
 int incomingByte = 0;
-
 void setup() {
+  
   // put your setup code here, to run once:
   Serial.begin(115200);
+  TCCR2B = TCCR2B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
+  //TCCR2B = TCCR2B & B11111000 | B00000010; // for PWM frequency of 3921.16 Hz
 }
 
 void loop() {
   if (Serial.available() > 0) {
-  // read the incoming byte:
     incomingByte = Serial.read();
-    analogWrite(analog_out,incomingByte);
-    Serial.println(incomingByte); 
+    analogWrite(analog_out,(incomingByte));
   }
- 
+  
 }
